ddd65b2220114f7670c4ab9427a875708dc4f1d6
Nata Miseyuk <nmtesthv@gmail.com>
2023-10-10 17:29:13 -0400

    add new file (#4)

--- lesson1/test_add_product_to_cart_from_card_item.py
+++ lesson1/test_add_product_to_cart_from_card_item.py
@@ -18,7 +18,9 @@ def test_add_item_to_the_cart():
     login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
     login_button.click()
 
-    text_before = driver.find_element(By.CSS_SELECTOR, "a[id='item_4_title_link'] > div[class='inventory_item_name']").text
+    text_before = driver.find_element(By.XPATH, '//div[contains(text(), "Sauce Labs Backpack")]').text
+    inventory_item_name = driver.find_element(By.XPATH, '//div[contains(text(), "Sauce Labs Backpack")]')
+    inventory_item_name.click()
 
     add_to_cart_button = driver.find_element(By.CSS_SELECTOR, 'button[data-test="add-to-cart-sauce-labs-backpack"]')
     add_to_cart_button.click()
@@ -26,7 +28,7 @@ def test_add_item_to_the_cart():
     cart = driver.find_element(By.CSS_SELECTOR, "a[class='shopping_cart_link']")
     cart.click()
 
-    text_after = driver.find_element(By.CSS_SELECTOR, "a[id='item_4_title_link'] > div[class='inventory_item_name']").text
+    text_after = driver.find_element(By.XPATH, '//div[contains(text(), "Sauce Labs Backpack")]').text
     assert text_before == text_after
 
     driver.quit()
